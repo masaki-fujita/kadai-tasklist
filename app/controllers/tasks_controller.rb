6d8ef568def2ad7fc3c58ec485820efa43bcb1a1
masaki-fujita <stalemate-pg@i.softbank.jp>
2019-04-18 12:55:44 +0000

    tough

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,7 +12,7 @@ class TasksController < ApplicationController
         @task = Task.new(task_params)
         if @task.save
             flash[:success] = "Taskが正常に投稿されました"
-            redirect :new
+            redirect_to @task
         else
             flash[:danger] = "Taskが投稿されませんでした"
             render :new
@@ -23,19 +23,19 @@ class TasksController < ApplicationController
     end 
     def update
         @task = Task.find(params[:id])
-        if @task.updata(task_params)
-            flash[:success] = "Taskは正常に更新されました"
+        if @task.update(task_params)
+            flash[:success] = "Task は正常に更新されました"
             redirect_to @task
         else
-            flash.now[:danger] = "Taskは更新されませんでした"
+            flash.now[:danger] = "Task は更新されませんでした"
             render :edit
         end 
     end 
     def destroy
         @task = Task.find(params[:id])
-        @message.destroy
-        flash[:success] = "Taskは正常に削除されました"
-        redirect_to task_url
+        @task.destroy
+        flash[:success] = "Task は正常に削除されました"
+        redirect_to tasks_url
     end 
     private
     def task_params
