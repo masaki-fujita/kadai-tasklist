266ebe71a82bbe73593d0cb906f8c766a0dd8281
masaki-fujita <stalemate-pg@i.softbank.jp>
2019-04-27 12:49:52 +0000

    user login

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
+  def index
+    @users = User.order(id: :desc).page(params[:page]).per(15)
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
