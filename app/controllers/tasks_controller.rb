b0fae67bc8769b9f4ab5c039e3d7573f5f1d2dae
masaki-fujita <stalemate-pg@i.softbank.jp>
2019-05-04 14:38:28 +0000

    after mentoring

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :destroy]
     def index
             @tasks = current_user.tasks.order(id: :desc).page(params[:page])
     end 
@@ -41,5 +42,11 @@ class TasksController < ApplicationController
     private
     def task_params
         params.require(:task).permit(:status, :content)
+    end 
+    def correct_user
+        @task = current_user.tasks.find_by(id: params[:id])
+        unless @task
+            redirect_to root_url
+        end
     end 
 end
