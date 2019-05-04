9374587f3046d954259f09ba2f7828879ece1be7
masaki-fujita <stalemate-pg@i.softbank.jp>
2019-05-04 12:08:35 +0000

    routes modify1

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  get 'tasks/create'
-  get 'tasks/destroy'
   get 'sessions/new'
-  get 'sessions/create'
   get 'sessions/destroy'
   get 'users/index'
   get 'users/show'
   get 'users/new'
-  get 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root to: "tasks#index"
     
